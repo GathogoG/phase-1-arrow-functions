eca3d904c76c8b23adcc4f2006d869f392b6bdc5
George Gathogo <gathogogeorge11@gmail.com>
2024-03-31 16:58:00 +0300

    arrow-functions

---
+++ index.js
@@ -0,0 +1,18 @@
+function divide(a=200, b=100) {
+  console.log(`dividing ${a}`);
+  console.log(`dividing ${b}`);
+  return a / b;
+}
+const square = (x=2)=> {
+  console.log(`square ${x}`);
+  return x*x
+}
+const add = (a=3,b=4) => {
+  console.log(`Adding ${a}`);
+  console.log(`Adding ${b}`);
+  return  a+b;
+}
+
+
+
+
